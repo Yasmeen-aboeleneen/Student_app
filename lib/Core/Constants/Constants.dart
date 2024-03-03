80affc8721fc6c47ceeb6b9c63b375cb0653bfe5
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-03-04 00:18:36 +0200

    ConstantsFile

---
+++ lib/Core/Constants/Constants.dart
@@ -0,0 +1,21 @@
+//Colors
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF345FB4);
+const Color kSecondaryColor = Color(0xFF6789CA);
+const Color kTextBlackColor = Color(0xFF313131);
+const Color kTextWhiteColor = Color(0xFFFFFFFF);
+const Color kOtherColor = Color(0xFFF4F6F7);
+const Color kContainerColor = Color(0xFF777777);
+const Color kTextLightColor = Color(0xFFA5A5A5);
+const Color kErrorBorderColor = Color(0xFFE74C3C);
+
+//Default Value
+const kDefaultPadding = 20;
+
+//Validation for mobile
+const String mobilePattern = r'(^(?:[+0]9)?[0-9{10,12}$]';
+
+//Validation for email
+const String emailPattern =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
